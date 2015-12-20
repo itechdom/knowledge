8e58c01b5f75f33fb9aace68760aae9b66f0c6ab
osama.alghanmi <osama.alghanmi@effectiveui.com>
2015-12-20 15:43:22 -0700

    status

--- code/Scala/Scala-starter/src/main/scala/test/Main.scala
+++ code/Scala/Scala-starter/src/main/scala/test/Main.scala
@@ -1,9 +1,18 @@
 package test
 
+class Hello(arg:String) {
+  def hello(arg:String) = {
+    arg
+  }
+  
+}
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    args.foreach(println) 
+    var he = new Hello("Hellooo")
+    println(he.hello("hi"))
   }
 
 }
