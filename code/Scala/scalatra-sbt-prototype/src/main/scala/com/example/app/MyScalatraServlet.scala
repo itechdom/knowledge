c57f2b29a52a332eefd3dac56f5f423294509ee7
osama.alghanmi <osama.alghanmi@effectiveui.com>
2015-12-20 16:03:39 -0700

    added scala-rest example

--- code/Scala/scalatra-sbt-prototype/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ code/Scala/scalatra-sbt-prototype/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -9,7 +9,7 @@ class MyScalatraServlet extends ScalatraServlet with ScalateSupport {
     <html>
       <body>
         <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        Say <a href="hello-scala">hello to Scalate</a>.
       </body>
     </html>
   }
